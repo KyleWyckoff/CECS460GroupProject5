cca74255eaba1e687452482adbfffe58e906fea1
KyleWyckoff <105688322+KyleWyckoff@users.noreply.github.com>
2025-04-03 12:01:55 -0700

    Update bist.v

--- bist.v
+++ bist.v
@@ -25,18 +25,18 @@ input [7:0] x1, x2, v, t, c,
 output reg failedFlag
 );
 
-reg [15:0] aTest, bTest, aTrue, bTrue;
-parameter k1 = 3, k2 = 5;
+reg equationFlag = 0;
+reg [15:0] aTrue, bTrue;
+wire [15:0] aTest, bTest;
 
 //Run a series of test on startup to ensure the wires for adder and multiplier are functional
-controller a0(.x1(x1),.x2(x2),.v(v),.t(t),.c(c),.clk(clk),.equationFlag(0),.A(aTest),.B(bTest));
-controller a1(.x1(x1),.x2(x2),.v(v),.t(t),.c(c),.clk(clk),.equationFlag(1),.A(aTest),.B(bTest));
+controller a0(.x1(x1),.x2(x2),.v(v),.t(t),.c(c),.clk(clk),.equationFlag(equationFlag),.A(aTest),.B(bTest));
 
-initial begin
-
-    aTrue = (x1 * k1) + (x2 * k2);
+always@(posedge clk)begin
+    equationFlag = ~equationFlag;
+    aTrue = (x1 * 3) + (x2 * 5);
     bTrue = (v * t) + c;
     failedFlag = (aTrue ^ aTest) | (bTrue ^ bTest); //failedFlag will be 1 if the test did not pass
-    
 end
+
 endmodule
