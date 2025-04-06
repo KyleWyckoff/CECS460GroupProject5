f2bc86ea518dabba47656f219eb53c737eaa03cd
Bronson Garel <78556724+bronsongarel@users.noreply.github.com>
2025-04-05 20:16:40 -0700

    Update top.v

--- top.v
+++ top.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 04/01/2025 07:23:29 PM
+// Create Date: 04/04/2025 10:53:25 PM
 // Design Name: 
-// Module Name: controller
+// Module Name: top
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -21,28 +21,55 @@
 
 
 module top(
-input clk,
-input reset,
-output bistSignal
-
-);
-reg bistPassed = 0;   
-reg equationFlag = 0;
-localparam period = 5;
-
- 
-/*At reset, run a short BIST routine to validate the multiplier and adder with known input-output pairs.  
-Indicate test pass/fail via an output signal.
-Normal operation begins only if the BIST passes.  */
-always @(posedge reset) begin
+    input clk,
+    input reset,
+    output bistSignal
+    );
     
-
-
-end   
- 
-always @(posedge clk)begin
-    equationFlag = ~equationFlag;
-
-end
+    reg bistPassed = 0;   
+    reg equationFlag = 0;
+    localparam period = 5;
+    
+    reg [7:0] x1, x2, v, t, c;
+    reg passed;
+    wire failed;
+    integer i;
+    
+    bist b1 (
+        .clk(clk), 
+        .x1(x1), 
+        .x2(x2),
+        .v(v), 
+        .t(t), 
+        .c(c), 
+        .failedFlag(failed)
+        );
+     
+    assign bistSignal = passed ? 1 : 0; // 1 if passed, 0 if failed
+    
+    /*At reset, run a short BIST routine to validate the multiplier and adder with known input-output pairs.  
+    Indicate test pass/fail via an output signal.
+    Normal operation begins only if the BIST passes.  */
+    always @(posedge reset) begin
+        for (i=1; i < 5; i++) begin
+            x1 <= i;
+            x2 <= i + 1;
+            v <= i;
+            t <= 2;
+            c <= i * 2;
+        end    
+    end   
+    
+    always @ (failed) begin
+        if (failed) begin
+            passed <= 0;
+        end else begin
+            passed <= 1; // Passed
+        end
+    end
+    
+    always @(posedge clk) begin
+        equationFlag <= ~equationFlag;
+    end
     
 endmodule
